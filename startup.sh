5ebbea18158b7a9cd13ab77fd8a945aaf8d60920
contactayush <131809320+contactayush@users.noreply.github.com>
2023-12-20 14:49:36 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+cd /mnt/servers/apache-tomcat-9.0.84/webapps && aws s3 cp s3://jenkins-deploy-buck/jenkins-war-folder/jenkins.war  .
 cd /mnt/servers/apache-tomcat-9.0.84/bin && ./startup.sh
+
